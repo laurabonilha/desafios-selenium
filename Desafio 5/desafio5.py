d02c90f97a5210e7cc7e1ba61bb912460e868eda
laurabonilha <bonilha.laura@gmail.com>
2025-02-11 21:40:33 -0300

    Melhoria na captura de nome e tipo do bitcoin em casos que nome e tipo sejam iguais; captura de valor; possibilidade de compra e variação 1 hora

--- Desafio 5/desafio5.py
+++ Desafio 5/desafio5.py
@@ -20,6 +20,8 @@ driver = webdriver.Chrome()
 wait = WebDriverWait(driver=driver, timeout=15, poll_frequency=1)
 driver.get('https://www.coingecko.com/pt')
 
+driver.maximize_window()
+
 # Aguardando elemento que indique carregamento total da página
 wait.until(EC.presence_of_element_located(locator=(By.XPATH, '/html/body/header/div[2]/div[3]/div/div[1]/a[1]/img[1]')))
 
@@ -34,3 +36,58 @@ var_btnTrocaMoeda.click()
 # Trocando a moeda para Real
 var_btnRealBr = driver.find_element(By.XPATH, "//div[contains(@class, 'setting-item') and contains(@class, 'tw-flex')]//span[text()='Brazil Real']")
 var_btnRealBr.click()
+
+# Alterando url para capturar 300 itens por página
+driver.get('https://www.coingecko.com/pt?items=300')
+
+# Aguardando o carregamento completo da tabela de resultados
+wait.until(EC.presence_of_element_located(locator=(By.XPATH, '/html/body/div[2]/main/div/div[5]/table')))
+
+# Encontrando todas as linhas de criptomoedas
+var_linhasCripto = driver.find_elements(By.XPATH, "//tr[contains(@class, 'tw-bg-white') and contains(@class, 'tw-text-sm')]")
+
+# Loop para capturar os dados em todas as páginas
+
+while True:
+    for criptomoeda in var_linhasCripto:
+        # Captura o nome completo da moeda (incluindo o tipo)
+        var_strElementoMoeda = criptomoeda.find_element(By.XPATH, ".//div[contains(@class, 'tw-text-gray-700')]")
+        var_strMoedaCompleto = var_strElementoMoeda.text.strip()
+
+        # Captura o tipo da moeda
+        try:
+            var_elementoTipoMoeda = criptomoeda.find_element(By.XPATH, ".//div[contains(@class, 'tw-block')]")
+            var_strTipoMoeda = var_elementoTipoMoeda.text.strip()
+        except:
+            var_strTipoMoeda = ""  # Se não encontrar o tipo, define como string vazia
+
+        # Remove o tipo da moeda apenas se estiver no final do nome (mesmo nome e tipo)
+        if var_strTipoMoeda and var_strMoedaCompleto.endswith(var_strTipoMoeda):
+            var_strNomeMoeda = re.sub(r"\b" + re.escape(var_strTipoMoeda) + r"\b\s*$", "", var_strMoedaCompleto).strip()
+        else:
+            var_strNomeMoeda = var_strMoedaCompleto  # Mantém o nome inalterado
+
+        # Verifica se há elemento de compra no site
+        var_elementoCompra = criptomoeda.find_element(By.XPATH, ".//td[contains(@class, '!tw-p-0') and contains(@class, 'tw-px-1') and contains(@class, 'tw-py-2.5')]")
+
+        try:
+            # Verifique se há um <div> dentro do <td>
+            var_elementoDivCompra = var_elementoCompra.find_element(By.XPATH, ".//div")
+            var_admiteCompra = True
+        except:
+            var_admiteCompra = False
+
+        # Captura o preço da moeda
+        var_strPrecoMoeda = criptomoeda.find_element(By.XPATH, ".//td[contains(@class, 'tw-text-end') and contains(@class, 'tw-px-1') and contains(@class, 'tw-text-gray-900')]").text
+
+        # Captura a variação em 1 hora
+        var_variacao1Hora = criptomoeda.find_element(By.XPATH, ".//span[contains(@class, 'gecko-up')]").text
+        
+        
+
+
+
+
+        
+
+
